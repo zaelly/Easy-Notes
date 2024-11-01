62f2d95f05ea1cffdb98f0a3bebb96b89dc95222
Zaelly Barbosa <zazabarbosa2@gmail.com>
2024-11-01 17:50:54 -0300

    add index nav

--- app/Views/navbar/index.php
+++ app/Views/navbar/index.php
@@ -21,9 +21,9 @@
     <link rel="stylesheet" href="<?= base_url('theme/plugins/icheck-bootstrap/icheck-bootstrap.css') ?>">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<nav class="navbar container-header navbar-expand-lg bg-body-tertiary">
+<nav class="navbar bg-primary container-header navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid nav-itens">
-        <a class="navbar-brand" href="#">Nota Fácil</a>
+        <a class="navbar-brand logo" href="#">Nota Fácil</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
